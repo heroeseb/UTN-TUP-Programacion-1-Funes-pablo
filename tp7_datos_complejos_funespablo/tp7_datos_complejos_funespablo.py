71e2d0df45b558fa01270586e4c85a1ebb873ed1
Pablo Sebastian Funes <109395899+heroeseb@users.noreply.github.com>
2026-04-22 15:31:56 -0300

    tp7 agregado revisado

--- tp7_datos_complejos_funespablo/tp7_datos_complejos_funespablo.py
+++ tp7_datos_complejos_funespablo/tp7_datos_complejos_funespablo.py
@@ -1,11 +1,4 @@
-# 1) Dado el diccionario precios_frutas 
-# precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva': 
-# 1450}
-# Añadir las siguientes frutas con sus respectivos precios: 
-# ● Naranja = 1200 
-# ● Manzana = 1500 
-# ● Pera = 2300
-
+# Ejercicio 1
 print('-'*50)
 print('Ejercicio 1')
 
@@ -26,39 +19,31 @@ print('Diccionario con nuevos valores: ')
 mostrar_elementos(precios_frutas)
 print('-'*50)
 
-# 2) Siguiendo con el diccionario precios_frutas que resulta luego de ejecutar el código 
-# desarrollado en el punto anterior, actualizar los precios de las siguientes frutas: 
-# ● Banana = 1330 
-# ● Manzana = 1700 
-# ● Melón = 2800
-
+# Ejercicio 2
 print('Ejercicio 2')
 
 precios_frutas['Banana']  = 1330
 precios_frutas['Manzana']  = 1700
 precios_frutas['Melón']  = 2800
 
-print('Diccionario con precios de Banana,Manzana y Melón actualizados:')
+print('Diccionario con precios de Banana, Manzana y Melón actualizados:')
 mostrar_elementos(precios_frutas)
 print('-'*50)
 
-# 3) Siguiendo con el diccionario precios_frutas que resulta luego de ejecutar el código 
-# desarrollado en el punto anterior, crear una lista que contenga únicamente las frutas sin los 
-# precios.
-
+# Ejercicio 3
 print('Ejercicio 3')
+
 def print_valores_lista(lista):
     print(*lista,sep=' , ')
+
 lista_values_precio_frutas = list(precios_frutas.keys())
 print('Lista de frutas: ')
 print_valores_lista(lista_values_precio_frutas)
 print('-'*50)
 
-# 4) Escribí un programa que permita almacenar y consultar números telefónicos. 
-# • Permití al usuario cargar 5 contactos con su nombre como clave y número como valor. 
-# • Luego, pedí un nombre y mostrale el número asociado, si existe.
-
+# Ejercicio 4
 print('Ejercicio 4')
+
 def input_int(mensaje,mensaje2):
     while True:
         opcion = input(mensaje).strip()
@@ -66,6 +51,7 @@ def input_int(mensaje,mensaje2):
             return opcion
         else:
             print(mensaje2)
+
 def input_string(mensaje,mensaje2):
     while True:
         opcion = input(mensaje).strip()
@@ -75,21 +61,24 @@ def input_string(mensaje,mensaje2):
             print(mensaje2)
 
 dic_numeros_tel = {}
+
 while True:
     cantidad_repeticiones = 5
     print('''
-        1. Almacenar Número telefónico
-        2. Consultar Número telefónico
+        1. Almacenar número telefónico
+        2. Consultar número telefónico
         3. Salir
         ''')
-    opcion = input_int('Elige una opcion: ','Ingrese un número correcto!')
+    opcion = input_int('Elige una opción: ','Ingrese un número correcto!')
+    
     match opcion:
         case '1':
             for _ in range(cantidad_repeticiones):
                 nombre = input_string('Ingrese el nombre del contacto: ','Ingrese un nombre correcto')
-                numero = input_int('Ingrese el número de telefono: ','Ingrese un número correcto!')
+                numero = input_int('Ingrese el número de teléfono: ','Ingrese un número correcto!')
                 dic_numeros_tel[nombre] = numero
                 print(f'Contacto {nombre} ingresado correctamente')
+
         case '2':
             if dic_numeros_tel:
                 nombre = input_string('Ingrese el nombre del contacto a buscar: ','Ingrese un nombre correcto')
@@ -99,39 +88,43 @@ while True:
                 else:
                     print('No tienes agendado a nadie con ese nombre!')
             else:
-                print('Aun no tienes ningun contacto agendado')
+                print('Aún no tienes ningún contacto agendado')
+
         case '3':
             print('Saliendo del programa')
             break
+
         case _:
             print('Ingrese una opción correcta!')
 
-# 5) Solicita al usuario una frase e imprime: 
-# • Las palabras únicas (usando un set). 
-# • Un diccionario con la cantidad de veces que aparece cada palabra
+# Ejercicio 5
 print('-'*50)
 print('Ejercicio 5')
-frase = input_string('Dime un frase: ','Solo se permiten letras!')
+
+frase = input_string('Dime una frase: ','Solo se permiten letras!')
 lista_frase = frase.lower().split()
 set_frase = set(lista_frase)
 dict_frase = {}
-print('Las palabras unicas son: ')
+
+print('Las palabras únicas son: ')
 for palabra in set_frase:
-    if not palabra == '':
+    if palabra != '':
         print(palabra,end=' ')
 print()
+
 for palabra in lista_frase:
     if palabra in dict_frase:
         dict_frase[palabra] += 1
     else:
         dict_frase[palabra] = 1
+
 print('La cantidad de veces que aparece cada palabra es: ')
 mostrar_elementos(dict_frase)
 
-# 6) Permití ingresar los nombres de 3 alumnos, y para cada uno una tupla de 3 notas. Luego, 
-# mostrá el promedio de cada alumno.
+# Ejercicio 6
 print('-'*50)
 print('Ejercicio 6')
+
 def input_entero_convert(mensaje,mensaje2):
     while True:
         opcion = input(mensaje).strip()
@@ -152,29 +145,22 @@ for i in range(3):
         if not nombre_alumno in dict_alumnos:
             for j in range(3):
                 while True:
-                    nota = input_entero_convert(f'Ingresa la nota {j+1}: ','Ingresa solo numeros!')
+                    nota = input_entero_convert(f'Ingresa la nota {j+1}: ','Ingresa solo números!')
                     if nota <= 10:
                         notas.append(nota)
                         break
                     else:
-                        print('El numero debe ser menor de 10')
+                        print('El número debe ser menor o igual a 10')
             dict_alumnos[nombre_alumno] = tuple(notas)
             break
         else:
-            print('El nombre ya esta en la lista!')
+            print('El nombre ya está en la lista!')
         
 print('Los promedios de los alumnos son: ')
 for k,v in dict_alumnos.items():
     print(f'- {k} : {sum(v)/len(v):.2f}')
 
-# 7) Se recibe el registro diario de asistencia a una capacitación en forma de lista.
-# En dicha lista pueden aparecer nombres repetidos, ya que una misma persona pudo haber 
-# asistido en más de una jornada.
-# • Mostrá la lista original de asistencias.
-# • Generá un conjunto (set) a partir de la lista y mostrar los empleados que asistieron al 
-# menos una vez (sin repetir nombres).
-# • Indicá cuántas veces asistió cada empleado a la capacitación.
-
+# Ejercicio 7
 print('-'*50)
 print('Ejercicio 7')
 
@@ -182,35 +168,36 @@ print('Lista de asistencia original: ')
 asistencias = [
     "Ana", "Luis", "Marta", "Ana", "Carlos",
     "Luis", "Ana", "Marta", "Pedro", "Luis",
-    "Sofia", "Carlos", "Ana", "Pedro", "Sofia",
+    "Sofía", "Carlos", "Ana", "Pedro", "Sofía",
     "Marta", "Luis", "Ana"
 ]
+
 print(*asistencias,sep=' , ')
 
 set_asistencia = set(asistencias)
+
 print('Empleados que asistieron al menos una vez: ')
 print(*set_asistencia,sep=' , ')
 
 for empleado in set_asistencia:
     print(f'Empleado {empleado} asistió: {asistencias.count(empleado)} veces')
 
-
-# 8) Armá un diccionario donde las claves sean nombres de productos y los valores su stock. 
-# Permití al usuario:
-# • Consultar el stock de un producto ingresado. 
-# • Agregar unidades al stock si el producto ya existe. 
-# • Agregar un nuevo producto si no existe.
+# Ejercicio 8
+print('-'*50)
+print('Ejercicio 8')
 
 productos_stock_diccionario = {}
+
 while True:
     print('''
-        Menu:
+        Menú:
         1. Consultar el stock de un producto
         2. Agregar unidades al stock
         3. Agregar nuevo producto
         4. Salir
         ''')
-    opcion = input_int('Ingresa una opción: ','Ingresa solo letras!')
+    opcion = input_int('Ingresa una opción: ','Ingresa solo números!')
+    
     match opcion:
         case '1':
             if productos_stock_diccionario:
@@ -218,14 +205,104 @@ while True:
                 if producto in productos_stock_diccionario:
                     print(f'El stock del producto {producto} es {productos_stock_diccionario[producto]}')
                 else:
-                    print('El producto no esta en stock')
+                    print('El producto no está en stock')
             else:
-                print('Aun no se ha inicializado el diccionario!')
+                print('Aún no se ha inicializado el diccionario!')
+
         case '2':
-            
+            if productos_stock_diccionario:
+                producto = input_string('Ingresa el nombre del producto: ','Ingresa un nombre correcto!')
+                if producto in productos_stock_diccionario:
+                    cantidad = input_entero_convert(f'Ingrese la cantidad de stock del producto {producto}: ','Ingrese un número correcto')
+                    productos_stock_diccionario[producto] = cantidad
+                    print('Stock agregado correctamente!')
+                else:
+                    print('El producto no está en stock')
+            else:
+                print('Aún no se ha inicializado el diccionario!')
+
         case '3':
-            pass
+            producto = input_string('Ingresa el nombre del producto que deseas agregar: ','Ingresa un nombre correcto!')
+            if not producto in productos_stock_diccionario:
+                cantidad = input_entero_convert(f'Ingrese la cantidad de stock del producto a ingresar: ','Ingrese un número correcto')
+                productos_stock_diccionario[producto] = cantidad
+                print('Producto agregado correctamente!')
+            else:
+                print('El producto ya está en inventario, si quieres agregar stock usa opción 2!')
+
         case '4':
-            pass
+            print('Saliendo del programa')
+            break
+
         case _:
-            pass
+            print('Ingresa una opción correcta!')
+
+# Ejercicio 9
+print('-'*50)
+print('Ejercicio 9')
+
+def esta_en_agenda(agenda_param,consultadia,consultahora):
+    for (dia, hora), evento in agenda_param.items():
+        if dia == consultadia and hora == consultahora:
+            return evento
+    return None
+
+def es_digito(dato):
+    if dato.replace(':','',1).isdigit():
+        return True
+    else:
+        return False
+
+agenda = {
+    ("lunes", "09:00"): "Clase de matemáticas",
+    ("lunes", "11:00"): "Reunión con equipo",
+    ("martes", "10:00"): "Estudiar programación",
+    ("miércoles", "15:00"): "Gimnasio",
+    ("jueves", "08:30"): "Consulta médica",
+    ("viernes", "20:00"): "Salida con amigos"
+}
+
+consulta_dia = input_string('¿Qué día deseas consultar en la agenda?: ','Ingresa solo letras!').lower()
+consulta_hora = input('Ingresa la hora que deseas consultar: ').strip()
+
+while not (len(consulta_hora[:2]) == 2 and len(consulta_hora[3:]) == 2 and consulta_hora.count(':') == 1 and es_digito(consulta_hora)):
+    print('Ingrese una hora correcta!')
+    consulta_hora = input('Ingresa la hora que deseas consultar: ')
+
+evento_agenda = esta_en_agenda(agenda,consulta_dia,consulta_hora)
+
+if evento_agenda:
+    print(f'El día {consulta_dia} a las {consulta_hora} tiene la actividad: {evento_agenda}')
+else:
+    print('Agenda vacía en ese día y horario!')
+
+# Ejercicio 10
+print('-'*50)
+print('Ejercicio 10')
+
+paises_capitales = {
+    "Argentina": "Buenos Aires",
+    "Chile": "Santiago",
+    "Uruguay": "Montevideo",
+    "Brasil": "Brasilia",
+    "Paraguay": "Asunción",
+    "Perú": "Lima",
+    "Bolivia": "Sucre",
+    "Colombia": "Bogotá",
+    "Ecuador": "Quito",
+    "Venezuela": "Caracas"
+}
+
+print('Diccionario original: ')
+mostrar_elementos(paises_capitales)
+
+def diccionario_invertido(diccionario):
+    dic_retorno = {}
+    for k,v in diccionario.items():
+        dic_retorno[v] = k
+    return dic_retorno
+
+nuevo_capitales_paises = diccionario_invertido(paises_capitales)
+
+print('Diccionario con valores invertidos: ')
+mostrar_elementos(nuevo_capitales_paises)
